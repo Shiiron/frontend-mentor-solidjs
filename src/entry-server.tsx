57c30822ca3a4c0515c5ab589fe08d8ffa1e1ee4
Loris Puech <puech.loris@gmail.com>
2024-01-13 11:41:45 +0100

    test server

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -4,7 +4,7 @@ export default createHandler(() => (
   <StartServer
     document={({ assets, children, scripts }) => (
       <html lang="en">
-        <head>
+        <head class="superhead">
           <meta charset="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link rel="icon" href="/favicon.ico" />
