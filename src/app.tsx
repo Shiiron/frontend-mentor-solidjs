6c537202d598ce7ce5e0480691e41c243cacced7
Loris Puech <puech.loris@gmail.com>
2024-01-13 11:37:47 +0100

    remove routes for deploy

--- src/app.tsx
+++ src/app.tsx
@@ -1,21 +1,25 @@
-// @refresh reload
-import { MetaProvider, Title } from "@solidjs/meta";
-import { Router } from "@solidjs/router";
-import { FileRoutes } from "@solidjs/start";
-import { Suspense } from "solid-js";
+import { For, createResource } from "solid-js";
 import "./app.css";
+import DataCardComponent from "./components/data-card/data-card";
+import ProfileCardComponent from "./components/profil-card/profile-card";
+import { ContextProvider } from "./contexts/context";
+import { dataService } from "./services/service";
 
 export default function App() {
+  const [data] = createResource<any[], string>(dataService)
+
   return (
-      <Router
-        root={(props) => (
-          <MetaProvider>
-            <Title>SOLIDJS first project</Title>
-            <Suspense>{props.children}</Suspense>
-          </MetaProvider>
-        )}
-      >
-        <FileRoutes />
-      </Router>
+      <main class="dark-blue">
+        <ContextProvider>
+          <div class="card-wrapper">
+            <ProfileCardComponent />
+            <For each={data()}>
+              {(data) => {
+                return (<DataCardComponent card={data} />)  
+              }}
+            </For>
+          </div>
+        </ContextProvider>
+      </main>
   );
 }
