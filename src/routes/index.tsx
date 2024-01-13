fdff17d852843c196ca17f012fa3c9cef42bb634
Loris Puech <puech.loris@gmail.com>
2024-01-13 14:10:45 +0100

    fix service

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,24 +1,124 @@
-import { createResource, For, Show } from "solid-js";
+import { For } from "solid-js";
 import DataCardComponent from "~/components/data-card/data-card";
 import ProfileCardComponent from "~/components/profil-card/profile-card";
 import { ContextProvider } from "~/contexts/context";
-import { dataService } from "~/services/service";
 
 export default function Home() {
-  const [data] = createResource(dataService)
+  const data = [
+    {
+      "title": "Work",
+      "timeframes": {
+        "daily": {
+          "current": 5,
+          "previous": 7
+        },
+        "weekly": {
+          "current": 32,
+          "previous": 36
+        },
+        "monthly": {
+          "current": 103,
+          "previous": 128
+        }
+      }
+    },
+    {
+      "title": "Play",
+      "timeframes": {
+        "daily": {
+          "current": 1,
+          "previous": 2
+        },
+        "weekly": {
+          "current": 10,
+          "previous": 8
+        },
+        "monthly": {
+          "current": 23,
+          "previous": 29
+        }
+      }
+    },
+    {
+      "title": "Study",
+      "timeframes": {
+        "daily": {
+          "current": 0,
+          "previous": 1
+        },
+        "weekly": {
+          "current": 4,
+          "previous": 7
+        },
+        "monthly": {
+          "current": 13,
+          "previous": 19
+        }
+      }
+    },
+    {
+      "title": "Exercise",
+      "timeframes": {
+        "daily": {
+          "current": 1,
+          "previous": 1
+        },
+        "weekly": {
+          "current": 4,
+          "previous": 5
+        },
+        "monthly": {
+          "current": 11,
+          "previous": 18
+        }
+      }
+    },
+    {
+      "title": "Social",
+      "timeframes": {
+        "daily": {
+          "current": 1,
+          "previous": 3
+        },
+        "weekly": {
+          "current": 5,
+          "previous": 10
+        },
+        "monthly": {
+          "current": 21,
+          "previous": 23
+        }
+      }
+    },
+    {
+      "title": "Self Care",
+      "timeframes": {
+        "daily": {
+          "current": 0,
+          "previous": 1
+        },
+        "weekly": {
+          "current": 2,
+          "previous": 2
+        },
+        "monthly": {
+          "current": 7,
+          "previous": 11
+        }
+      }
+    }
+  ];
 
   return (
     <main class="dark-blue">
       <ContextProvider>
         <div class="card-wrapper">
           <ProfileCardComponent />
-          <Show when={data()} fallback="loading...">
-            <For each={data()}>
+            <For each={data}>
               {(data) =>
                 <DataCardComponent card={data} />
               }
             </For>
-          </Show>
         </div>
       </ContextProvider>
     </main>
