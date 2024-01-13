49cdf02e5a2fd12061ae0dea56c930c47a3f0b5a
Loris Puech <puech.loris@gmail.com>
2024-01-13 12:35:42 +0100

    fox node

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -1,10 +1,11 @@
-import { createHandler, StartServer } from "@solidjs/start/server";
+import { StartServer } from "@solidjs/start/server";
+import { createHandler } from "@solidjs/start/entry";
 
 export default createHandler(() => (
   <StartServer
     document={({ assets, children, scripts }) => (
       <html lang="en">
-        <head class="superhead">
+        <head>
           <meta charset="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link rel="icon" href="/favicon.ico" />
@@ -20,3 +21,5 @@ export default createHandler(() => (
     )}
   />
 ));
+
+
